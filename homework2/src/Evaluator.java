fb7eb49f6ad048ab05586a8b3fdd57295ae9475e
Don Rowe <rowedonalde@gmail.com>
2012-04-08 14:37:01 -0700

    Initialized Evaluator.java

---
+++ homework2/src/Evaluator.java
@@ -0,0 +1,11 @@
+/**
+ * Evaluator.java
+ *
+ * This class contains the logic necessary, specifically the evaluate() method,
+ * that allows PublicEvaluator to work.
+ */
+
+public class Evaluator
+{
+    
+}
