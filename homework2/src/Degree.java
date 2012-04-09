d5f2f6116f43ee32a503894ec7f6d3220364381e
Don Rowe <rowedonalde@gmail.com>
2012-04-08 17:35:13 -0700

    Added constructor, getter, and evaluator for Degree.java

--- homework2/src/Degree.java
+++ homework2/src/Degree.java
@@ -6,5 +6,26 @@
 
 public class Degree
 {
+    private int exponent;
     
+    /**
+     * Constructs a new Degree object with the given exponent:
+     */
+    public Degree(int exponent)
+    {
+        this.exponent = exponent;
+    }
+    
+    public int getExponent()
+    {
+        return this.exponent;
+    }
+    
+    /**
+     * Evaluate this Degree with the given value for x:
+     */
+    public double evaluate(int x)
+    {
+        return Math.pow(x, this.exponent);
+    }
 }
