025b3a9f7b6668b72c06b27c6a16c63431158006
Don Rowe <rowedonalde@gmail.com>
2012-04-09 23:34:46 -0700

    Initialized PolynomialEvaluator.java

---
+++ homework2/src/PolynomialEvaluator.java
@@ -0,0 +1,16 @@
+/**
+ * PolynomialEvaluator.java
+ *
+ * Runs the polynomial evaluator as a command-line application
+ * 
+ * Syntax:
+ *     java PolynomialEvaluator expression xValue
+ *
+ * where expression is a single-variable (specifically, x) polynomial
+ * and xValue is an integer value for x.
+ */
+
+public class PolynomialEvaluator
+{
+    
+}
