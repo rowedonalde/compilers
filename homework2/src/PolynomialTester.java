3d29a7b98fc8c6a51d21526d3aeafb03a9ca98e0
Don Rowe <rowedonalde@gmail.com>
2012-04-10 01:54:53 -0700

    Added testDegreeZero() method to PolynomialTester.java

--- homework2/src/PolynomialTester.java
+++ homework2/src/PolynomialTester.java
@@ -4,7 +4,27 @@
  * Test suite for the polynomial evaluator
  */
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class PolynomialTester
 {
-
+    /**
+     * Evaluate the degree x^0: should always evaluate to 1,
+     * regardless of the value of x.
+     */
+    @Test
+    public void testDegreeZero()
+    {
+        Degree deg = new Degree(0);
+        assertThat(deg.getExponent(), is(0));
+        assertThat(deg.evaluate(0), is(1));
+        assertThat(deg.evaluate(1), is(1));
+        assertThat(deg.evaluate(-1), is(1));
+        assertThat(deg.evaluate(2), is(1));
+        assertThat(deg.evaluate(1000), is(1));
+    }
 }
