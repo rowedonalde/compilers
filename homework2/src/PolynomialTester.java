ea4701d5b0fb0cc3bdc560ac7fe9544705addf04
Don Rowe <rowedonalde@gmail.com>
2012-04-10 02:45:49 -0700

    In PolynomialTester.java:     * Added testDegreeNoArgument() method     * Added testExpression() method     * Correct integer evaluation() results to be doubles, e.g., 1.0, not 1

--- homework2/src/PolynomialTester.java
+++ homework2/src/PolynomialTester.java
@@ -9,6 +9,8 @@ import static org.junit.Assert.assertThat;
 
 import org.junit.Before;
 import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PolynomialTester
 {
@@ -21,11 +23,11 @@ public class PolynomialTester
     {
         Degree deg = new Degree(0);
         assertThat(deg.getExponent(), is(0));
-        assertThat(deg.evaluate(0), is(1));
-        assertThat(deg.evaluate(1), is(1));
-        assertThat(deg.evaluate(-1), is(1));
-        assertThat(deg.evaluate(2), is(1));
-        assertThat(deg.evaluate(1000), is(1));
+        assertThat(deg.evaluate(0), is(1.0));
+        assertThat(deg.evaluate(1), is(1.0));
+        assertThat(deg.evaluate(-1), is(1.0));
+        assertThat(deg.evaluate(2), is(1.0));
+        assertThat(deg.evaluate(1000), is(1.0));
     }
     
     /**
@@ -36,12 +38,12 @@ public class PolynomialTester
     {
         Degree deg = new Degree(1);
         assertThat(deg.getExponent(), is(1));
-        assertThat(deg.evaluate(1), is(1));
-        assertThat(deg.evaluate(0), is(0));
-        assertThat(deg.evaluate(-1), is(-1));
-        assertThat(deg.evaluate(2), is(2));
-        assertThat(deg.evaluate(10), is(10));
-        assertThat(deg.evaluate(-10), is(-10));
+        assertThat(deg.evaluate(1), is(1.0));
+        assertThat(deg.evaluate(0), is(0.0));
+        assertThat(deg.evaluate(-1), is(-1.0));
+        assertThat(deg.evaluate(2), is(2.0));
+        assertThat(deg.evaluate(10), is(10.0));
+        assertThat(deg.evaluate(-10), is(-10.0));
     }
     
     /**
@@ -52,13 +54,13 @@ public class PolynomialTester
     {
         Degree deg = new Degree(2);
         assertThat(deg.getExponent(), is(2));
-        assertThat(deg.evaluate(0), is(0));
-        assertThat(deg.evaluate(1), is(1));
-        assertThat(deg.evaluate(-1), is(1));
-        assertThat(deg.evaluate(2), is(4));
-        assertThat(deg.evaluate(-2), is(4));
-        assertThat(deg.evaluate(10), is(100));
-        assertThat(deg.evaluate(-10), is(100));
+        assertThat(deg.evaluate(0), is(0.0));
+        assertThat(deg.evaluate(1), is(1.0));
+        assertThat(deg.evaluate(-1), is(1.0));
+        assertThat(deg.evaluate(2), is(4.0));
+        assertThat(deg.evaluate(-2), is(4.0));
+        assertThat(deg.evaluate(10), is(100.0));
+        assertThat(deg.evaluate(-10), is(100.0));
     }
     
     /**
@@ -89,11 +91,11 @@ public class PolynomialTester
          Degree deg = new Degree(2);
          Monomial mon = new Monomial(1, deg);
          assertThat(mon.getCoefficient(), is(1));
-         assertThat(mon.evaluate(0), is(0));
-         assertThat(mon.evaluate(1), is(1));
-         assertThat(mon.evaluate(2), is(4));
-         assertThat(mon.evaluate(-1), is(1));
-         assertThat(mon.evaluate(-2), is(4));
+         assertThat(mon.evaluate(0), is(0.0));
+         assertThat(mon.evaluate(1), is(1.0));
+         assertThat(mon.evaluate(2), is(4.0));
+         assertThat(mon.evaluate(-1), is(1.0));
+         assertThat(mon.evaluate(-2), is(4.0));
      }
      
      /**
@@ -105,12 +107,50 @@ public class PolynomialTester
          Degree deg = new Degree(2);
          Monomial mon = new Monomial(2, deg);
          assertThat(mon.getCoefficient(), is(2));
-         assertThat(mon.evaluate(0), is(0));
-         assertThat(mon.evaluate(1), is(2));
-         assertThat(mon.evaluate(2), is(8));
-         assertThat(mon.evaluate(-1), is(2));
-         assertThat(mon.evaluate(-2), is(4));
+         assertThat(mon.evaluate(0), is(0.0));
+         assertThat(mon.evaluate(1), is(2.0));
+         assertThat(mon.evaluate(2), is(8.0));
+         assertThat(mon.evaluate(-1), is(2.0));
+         assertThat(mon.evaluate(-2), is(4.0));
      }
+     
+     /**
+      * Test that no argument in Monomial constructor throws error:
+      */
+     @Test(expected = IllegalArgumentException.class)
+     public void testDegreeNoArgument()
+     {
+         Monomial mon = new Monomial();
+     }
+     
+     /**
+      * Test some evaluations of 3x^2 + 2x + 1
+      */
+     @Test
+     public void testExpression()
+     {
+         Degree degA = new Degree(2);
+         Monomial a = new Monomial(3, degA);
+         Degree degB = new Degree(1);
+         Monomial b = new Monomial(2, degB);
+         Degree degC = new Degree(0);
+         Monomial c = new Monomial(1, degC);
+         List<Monomial> mons = new ArrayList<Monomial>();
+         mons.add(a);
+         mons.add(b);
+         mons.add(c);
+         Expression exp = new Expression(mons);
+         
+         assertThat(exp.evaluate(0), is(1.0));
+         assertThat(exp.evaluate(1), is(6.0));
+         assertThat(exp.evaluate(2), is(17.0));
+         assertThat(exp.evaluate(-1), is(2.0));
+         assertThat(exp.evaluate(-2), is(9.0));
+     }
+     
+     /**
+      * 
+      */
 }
 
 
