0668b5aa5baad6a2c7fdd41ee914edb23baeadec
Don Rowe <rowedonalde@gmail.com>
2012-04-09 23:51:38 -0700

    Added main method to PolynomialEvaluator.java

--- homework2/src/PolynomialEvaluator.java
+++ homework2/src/PolynomialEvaluator.java
@@ -12,5 +12,24 @@
 
 public class PolynomialEvaluator
 {
-    
+    public static void main(String[] args)
+    {
+        //Grab the polynomial and the x-value from the
+        //command line args
+        String polynomial = args[0];
+        int xValue = Integer.parseInt(args[1]);
+        
+        //Build the parser for the given polynomial String:
+        Parser parser = new Parser(java.io.StringReader(polynomial));
+        
+        //The actual expression tree object, which we can evaluate
+        //with the x-value:
+        Expression exp = parser.parse();
+        
+        //Evaluate the expression:
+        double solution = exp.evaluate(xValue);
+        
+        //Print the solution to stdout:
+        System.out.println(solution);
+    }
 }
